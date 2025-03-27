048e4c16a164ec539a31c5b70a99cbf984537631
sicabc-rh <rachelh@sicabc.ca>
2025-03-27 15:20:11 -0700

    Fix CSV::Table concat error

--- lib/removed.rb
+++ lib/removed.rb
@@ -7,12 +7,12 @@ def add_lines_to_final_output(final_output, old_file, updated_file)
   old_file_data = CSV.read(old_file, headers: true)
 
   # Get the email addresses from the final output and old file
-  final_emails = final_file_data['Email']  # Assuming 'Email' is the column header for email addresses
+  final_emails = final_file_data['Email Address']  # Assuming 'Email' is the column header for email addresses
   added_rows = []
 
   # Iterate through the rows in the old file
   old_file_data.each do |row|
-    email = row['Email']  # Assuming 'Email' is the column header for email addresses
+    email = row['Email Address']  # Assuming 'Email' is the column header for email addresses
     
     # If the email from the old file is not found in the final output, add the row
     unless final_emails.include?(email)
@@ -22,7 +22,10 @@ def add_lines_to_final_output(final_output, old_file, updated_file)
   end
 
   # Append the rows from the old file to the final output
-  final_file_data.concat(added_rows)
+  final_file_array = final_file_data.to_a
+  final_file_array.concat(added_rows)
+
+  final_file_data = CSV::Table.new(final_file_array)
 
   # Write the modified data to the updated file
   CSV.open(updated_file, 'wb') do |csv|
